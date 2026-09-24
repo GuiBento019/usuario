a06b7bbba726f934e0a060128ada7fbd3eaf2da0
Guilherme Bento <xgbs360@gmail.com>
2026-09-24 00:08:29 -0300

    adicionados métodos: get, delete e login

--- src/main/java/com/guilherme/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/guilherme/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.guilherme.usuario.infrastructure.repository;
 
-import com.guilherme.aprendendo_spring.infrastructure.entity.Endereco;
+
+import com.guilherme.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
